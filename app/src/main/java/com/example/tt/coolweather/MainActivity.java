2990aac5786f3af5241cb0ca4b3573822402fc29
Tony <tony@gmail.com>
2017-12-03 22:19:29 +0800

    加入创建数据库和表的各项配置。

--- app/src/main/java/com/example/tt/coolweather/MainActivity.java
+++ app/src/main/java/com/example/tt/coolweather/MainActivity.java
@@ -7,25 +7,5 @@ import org.litepal.crud.DataSupport;
 
 public class MainActivity extends AppCompatActivity {
 
-    public class Province extends DataSupport{
-        private int id;
-        private  String provinceName;
-        private int provinceCode;
-        public int getId(){
-            return id;
-        }
-        public void setId(int id){
-            this.id = id;
-        }
-        public String getProvinceName(){
-            return provinceName;
-        }
-        public void setProvinceName(String provinceName){
-            this.provinceName = provinceName;
-        }
-        public int getProvinceCode(){
-            return provinceCode;
-        }
-        private void setProvinceCode
-    }
+
 }
